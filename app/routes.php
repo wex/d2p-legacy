30b0ef3751e092ee642e12d735c4bf59720e143a
Niko Hujanen <niko.hujanen@gmail.com>
2018-11-15 04:13:20 +0200

    Striping stuff to get template up

--- app/routes.php
+++ app/routes.php
@@ -3,16 +3,5 @@
 return function(\Aura\Router\Map $router) {
     
 
-    $router->get('foo', '/asdf/{bar}', 'FooController@index');
-
-    $router->get('test', '/test', function($request, $response) {
-
-        var_dump( $this );
-        var_dump( $request );
-        var_dump( $response );
-        $response->getBody()->write('This is test!');
-
-        return $response;        
-    });
-
+    
 };
